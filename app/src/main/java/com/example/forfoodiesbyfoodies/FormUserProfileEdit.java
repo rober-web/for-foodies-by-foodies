d034c32a211d1571a1bf853d2adbe15eabdb01ac
rober-web <45374701+rober-web@users.noreply.github.com>
2021-10-23 18:49:19 +0100

    Created the basic pages for implementing layouts and logic.

---
+++ app/src/main/java/com/example/forfoodiesbyfoodies/FormUserProfileEdit.java
@@ -0,0 +1,14 @@
+package com.example.forfoodiesbyfoodies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormUserProfileEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_user_profile_edit);
+    }
+}
---
+++ app/src/main/java/com/example/forfoodiesbyfoodies/PageRestaurantSingle.java
@@ -0,0 +1,14 @@
+package com.example.forfoodiesbyfoodies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PageRestaurantSingle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_restaurant_single);
+    }
+}
